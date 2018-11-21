9ea4017489e5a232d000bd819598816ac88ab695
tomas99batista <34144094+tomas99batista@users.noreply.github.com>
2018-11-21 11:22:47 +0000

    Removed pastes

--- T_Pratico_02/filtro.m
+++
@@ -1,9 +0,0 @@
-function F_filtrado = filtro(F,fa,upperbound, lowerbound)
-    fil = zeros(1,length(fa));
-    fil(fa>=-1*upperbound) = 1;
-    fil(fa> -1* lowerbound) = 0;
-    fil(fa>=lowerbound) = 1;
-    fil(fa>=upperbound) = 0;
-    F_filtrado = F.*fil';
-    plot(fa,fil);
-end
